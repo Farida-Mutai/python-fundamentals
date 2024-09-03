17a573678549481052982f47e5167f7c036b98c4
Farida Mutai <faridamutai52@gmail.com>
2024-09-03 13:05:00 +0300

    reverse

---
+++ reverse_string.py
@@ -0,0 +1,7 @@
+def reverse_string(text):
+    return text[::-1]
+result = reverse_string("hello")
+print(result)  # Output will be "olleh"
+
+result = reverse_string("mom")
+print(result)  # Output will be "mom"
